46e61d3a0119401ab95e2f2e0cd156a0bc1a47cf
gskerry <gregory.skerry@gmail.com>
2016-02-22 17:41:04 +0000

    scrap callback

--- casper_get.js
+++ casper_get.js
@@ -5,23 +5,23 @@ var casper = require('casper').create({
 });
 var fs = require('fs');
 
-var cb = function (){
-    this.echo("I've waited for 2 seconds.");
-}
-
-casper.start('http://www.pbs.org/wgbh/frontline/watch/')
-
-casper.then(function(){
-    this.wait(2000, cb);
-})
-
 var dostuff = function(){
     var page = this.getCurrentUrl()
     console.log('page:', page);
 }
 
+casper.start('http://www.pbs.org/wgbh/frontline/watch/')
+
 casper.then(function(){
-    
+    this.wait(2000, function() {
+        this.echo("I've waited for 2 seconds.");
+        if (this.exists('#pbs-modal-overlay'))
+        this.click('#pbs-close-popup');
+    });
+})
+
+casper.then(function(){
+    dostuff();
 })
 
 casper.run();
