2bf4354df8817cc0aa23c37804d4822e1a8a7356
gskerry <gregory.skerry@gmail.com>
2016-02-22 10:40:34 -0500

    set up

---
+++ app.js
@@ -0,0 +1,40 @@
+
+var express = require('express');
+var morgan = require('morgan')
+// var bodyParser = require('body-parser')
+// var routes = require('./routes/')//(io)
+var sass = require('node-sass');
+var sassMiddleware = require('node-sass-middleware')
+
+// var mongo_api = require('./frontline3')
+var mongo_api = require('./mongo_API')
+
+var app = express();
+
+require('./swigConfig')(app)
+
+var server = app.listen(1400, '0.0.0.0', function() {
+	console.log("server listening...")
+} );
+
+app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(
+  sassMiddleware({
+    src: __dirname + '/assets', //where the sass files are 
+    dest: __dirname + '/public', //where css should go
+    debug: true
+  })
+);
+app.use(express.static(__dirname + '/public'));
+
+app.use( '/', function(req, res){
+	console.log("App active - testing.")
+	var list = mongo_api.list();
+	res.render('index',{frontlines: list});
+});
+
+
+
+// change2
