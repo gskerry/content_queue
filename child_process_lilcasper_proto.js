7c0c7644a12aee84f4bf6ac2b855b3e34357c3d2
gskerry <gregory.skerry@gmail.com>
2016-02-22 19:00:04 +0000

    lilcasper child - dataType fix

--- child_process_lilcasper_proto.js
+++ child_process_lilcasper_proto.js
@@ -1,13 +1,18 @@
 
+var date = "";
+
 var spawn_promise = function(){
 	return new Promise(function(resolve, reject){
 
 		var spawn = require('child_process').spawn;
 
-		var lilcasper = spawn('casperjs', ['--version'])
+		var lilcasper = spawn('casperjs', ['scrape_traverse.js'])
 
 		lilcasper.stdout.on('data', (data) => {
+		  console.log("typeof data: ",typeof data)
+		  console.log("data: ",data)
 		  console.log(`stdout: ${data}`);
+		  date = data.toString();
 		});
 
 		lilcasper.stderr.on('data', (data) => {
@@ -17,7 +22,7 @@ var spawn_promise = function(){
 
 		lilcasper.on('close', (code) => {
 		  console.log(`child process exited with code ${code}`);
-		  resolve(code);
+		  resolve(date);
 		});
 
 	});
