87839a90b0dffff6048b0a2d7ecd23a184b0f07e
gskerry <gregory.skerry@gmail.com>
2016-02-22 18:41:50 +0000

    return casper to repeat loop; placeholder casper promise in scrape/seed; hardcode file locations

--- casper_get.js
+++ casper_get.js
@@ -1,3 +1,4 @@
+
 var casper = require('casper').create({
     // verbose: true,
     // logLevel: "debug"
@@ -15,63 +16,56 @@ var mins = d.getMinutes();
 var mydate = yr+"-"+month+"-"+day+"--"+hrs+mins
 console.log(mydate);
 
-var traverse_promise = function(){
-    console.log("traverse_promise called.")
-    return new Promise(function(resolve, reject){
+casper.start('http://www.pbs.org/wgbh/frontline/watch/')
 
-        casper.on("error", function(){
-            console.log("casper error: ",error)
-            reject(error)
-        });
-
-        casper.start('http://www.pbs.org/wgbh/frontline/watch/')
-
-        casper.then(function(){
-            this.wait(2000, function() {
-                this.echo("I've waited for 2 seconds.");
-                if (this.exists('#pbs-modal-overlay')){
-                    console.log("PopUp Detected.")
-                    this.click('.closeBtn');    
-                }
-                
-            });
-        }).repeat(5, function(){
-            
-            if (this.exists('#pbs-modal-overlay')){
-                    console.log("PopUp Detected.")
-                    this.click('.closeBtn');
-            }
-
-            var page = this.getCurrentUrl()
-            console.log('page:', page);
-
-            this.capture(mydate+'/frontline-'+ i +'.png', {
-                top: 0,
-                left: 0,
-                width: 500,
-                height: 400
-            });
-
-            var code = this.getHTML()
-
-            this.download(page, mydate+'/index-'+i+'.html')
-
-            this.click('a.pagination__link.pagination__page.pagination__next');
-
-            i++;
-
-        });
-
-        casper.then(function() {
-            resolve();
-            exit();
-        });
-
-        casper.run();
-    }); // close promise
-} // close traverse_promise
-
-
-traverse_promise.then(function(){
-    console.log("all done.")
+casper.then(function(){
+    this.wait(2000, function() {
+        // this.echo("I've waited for 2 seconds.");
+        if (this.exists('#pbs-modal-overlay')){
+            // console.log("PopUp Detected.")
+            this.click('.closeBtn');    
+        }
+        
+    });
 })
+
+casper.then(function(){
+    if (this.exists('a.pagination__link.pagination__page.pagination__next')){
+        i++;
+        this.click('a.pagination__link.pagination__page.pagination__next');
+    }
+})
+
+.repeat(5, function(){
+    
+    if (this.exists('#pbs-modal-overlay')){
+            // console.log("PopUp Detected.")
+            this.click('.closeBtn');
+    }
+
+    var page = this.getCurrentUrl()
+    // console.log('page:', page);
+
+    this.capture(mydate+'/images/frontline-'+ i +'.png', {
+        top: 0,
+        left: 0,
+        width: 500,
+        height: 400
+    });
+
+    var code = this.getHTML()
+
+    this.download(page, mydate+'/html/index-'+i+'.html')
+
+    this.click('a.pagination__link.pagination__page.pagination__next');
+
+    i++;
+
+});
+
+casper.then(function() {
+    // console.log("all done.")
+    exit()
+});
+
+casper.run();
--- scrape_and_seed.js
+++ scrape_and_seed.js
@@ -1,5 +1,8 @@
+
 var fs = require('fs');
 var assert = require('assert');
+// var exec = require('child_process').exec
+var spawn = require('child_process').spawn;
 
 var request = require('request');
 var cheerio = require('cheerio');
@@ -8,52 +11,53 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 
-/*
-Run casper_get manually...
-Hardcode dir "today"
-*/
-
-var path = 'today'
-var files = fs.readdirSync(path)
-console.log(files);
+// var datepath = "";
+// Hardcode... pending casper sub-process and data transmission
+var datepath = "2016-1-21--2050";
 
 var outer_ray = [];
 
-var upsert_promise = function(db, ref, doc) {
+// **********
+// promises
+// **********
 
+var casper_promise = function(){
 	return new Promise(function(resolve, reject){
-		db.collection('seed_2_tests').replaceOne( 
-			{ href: ref },
-			doc,
-			{ upsert: true },
-			function(err, result) {
-				if(err){
-					reject(err)
-				} else {
-					console.log("resolved. doc inserted.");	
-					resolve(result)	
-				} //close if
-			} // close replace cb
-		); // close mongo.replace
+
+		resolve("done.")
+
+	});
+};
+
+var upsert_promise = function(db, ref, doc) {
+	return new Promise(function(resolve, reject){
+		try {
+			db.collection('seed_2_tests').replaceOne( 
+				{ "href" : ref },
+				doc,
+				{ upsert: true },
+				function(err, result) {
+					if(err){
+						reject(err)
+					} else {
+						console.log("resolved. doc inserted.");	
+						resolve(result)	
+					} //close if
+				} // close replace cb
+			); // close mongo.replace
+		}
+		catch (e) {
+			console.log (e)
+		}
+
 	}) // close promise
 }; // close upsert_promise
 
-var scrape_promise_test = function(file){
-	return new Promise(function(resolve, reject){
-		fs.exists(path+'/'+file, (exists) => {
-			if(exists === false){
-				reject('doesnt exist')
-			} else {
-				resolve(file);
-			}
-		});
-	}); //close promise
-} // close fnc
 
 var scrape_promise_too = function(file){
 	return new Promise(function(resolve, reject){
 
-		fs.readFile(path+'/'+file, 'utf8', function(err, data){
+		fs.readFile(datepath+'/html/'+file, 'utf8', function(err, data){
 			if(err){ 
 				reject(err) 
 			} else {
@@ -83,36 +87,56 @@ var scrape_promise_too = function(file){
 
 			} // close IF
 		}); // close read
+
 	}); //close promise
 } // close fnc
 
-var outer_promise_ray = files.map(scrape_promise_too);
-console.log("outer_promise_ray: ",outer_promise_ray);
 
-Promise.all(outer_promise_ray)
-	.then(function(results){
-		// console.log("outer_promise_ray results: ",results)
-		// console.log("outer_ray: ",outer_ray)
-		console.log("outer_ray sample: ",outer_ray[0])
+// **************
+// process chain
+// **************
 
-		MongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
 
-			var inner_promise_ray = outer_ray.map( (doc) => { return upsert_promise(db, doc.ref, doc)} )
-			console.log("inner_promise_ray sample: ", inner_promise_ray[0])
+casper_promise()
+	.then(function(result){
 
-			Promise.all(inner_promise_ray)
-			.then(function(){
-					console.log("close db here...")
-					db.close();
-				}
-			)
-		});
+		console.log("casper promise gives : ",result)
 
-		var storeStr = JSON.stringify(outer_ray)
-		fs.	appendFile('frontlines_ALL.json', storeStr, 'utf8', (err) => {
-			if (err) throw err;
-			console.log("data appended to file");
-		});
+		var files = fs.readdirSync(datepath+'/html')
+		console.log(files);
+
+		var outer_promise_ray = files.map(scrape_promise_too);
+		console.log("outer_promise_ray: ",outer_promise_ray);
+
+		Promise.all(outer_promise_ray)
+			.then(function(results){
+				// console.log("outer_promise_ray results: ",results)
+				// console.log("outer_ray: ",outer_ray)
+				console.log("outer_ray sample: ",outer_ray[0])
+
+				MongoClient.connect(url, function(err, db) {
+					assert.equal(null, err);
+
+					var inner_promise_ray = outer_ray.map( (doc) => { return upsert_promise(db, doc.ref, doc)} )
+					console.log("inner_promise_ray sample: ", inner_promise_ray[0])
+
+					Promise.all(inner_promise_ray)
+					.then(function(){
+							console.log("close db here...")
+							db.close();
+						}
+					)
+				});
+
+				var storeStr = JSON.stringify(outer_ray)
+				fs.	appendFile('frontlines_ALL.json', storeStr, 'utf8', (err) => {
+					if (err) throw err;
+					console.log("data appended to file");
+				});
+			})
+
+	})
+	.catch(function(error){
+		console.log("promise catch error: ", error)	
 	})
 
